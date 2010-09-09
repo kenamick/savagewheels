e94bf8e47dfa9573cbcc82b5bfc29a50f8cad85a
petarov <petar.petrov.georgiev@gmail.com>
2010-09-09 21:01:57 +0000

    * modified - slight updates

--- src/CKdf.h
+++ src/CKdf.h
@@ -32,19 +32,18 @@
 #define KDF_VER_MIN			1
 #define SET_VER(x)			{ (x)->maj_ver = KDF_VER_MAJ; (x)->min_ver = KDF_VER_MIN; }
 
-#define KDF_SUCCESS						 1
-#define KDF_ERROR_EMPTYDIR				10
-#define KDF_ERROR_OPENTOSAVE			11
-#define KDF_ERROR_OPENTOREAD			12
-#define KDF_ERROR_NOHEADER				13
+#define KDF_SUCCESS			1
+#define KDF_ERROR_EMPTYDIR		10
+#define KDF_ERROR_OPENTOSAVE		11
+#define KDF_ERROR_OPENTOREAD		12
+#define KDF_ERROR_NOHEADER		13
 #define KDF_ERROR_UNSUPPORTED_VERSION	14
-#define KDF_ERROR_FILEDOESNOTEXIST		-1
+#define KDF_ERROR_FILEDOESNOTEXIST	-1
 
 class CKdf_Packeger 
 {
 
 private:
-
 	struct kdf_file 
 	{
 		char  filename[64];
@@ -59,13 +58,11 @@ private:
 	};
 
 
-	kdf_file   *pfiles;						// array of file-info structures
-	char       **paths;						// array of file-paths
-	int		   num_files;					// files added/opened
+	kdf_file   *pfiles;				// array of file-info structures
+	char       **paths;				// array of file-paths
+	int	   num_files;				// files added/opened
 	char	   kdf_pack_name[255];			// name of the packet to save/open
-	
-	bool	   logfile;						// enable log
-
+	bool	   logfile;				// enable log
 
 public:
 	CKdf_Packeger();
@@ -81,7 +78,6 @@ public:
 	long GetFilePosition( int file_index );
 	long GetFileSize( const char *file_name );
 	long GetFileSize( int file_index );
-
 };
 
 
